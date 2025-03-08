4da3a4db8e71bcbd48e8d09ee320a322dc94ac08
Melissa Dennis <melissadennis120@gmail.com>
2025-03-08 07:35:46 -0400

    Add TodoistCloneAdvanced/.eslintrc.js 1741433745594

---
+++ TodoistCloneAdvanced/.eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'react-native/react-native': true,
+  },
+  rules: {
+    // TypeScript specific rules
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+    // React specific rules
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // General rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unused-vars': 'off', // Using TypeScript's no-unused-vars instead
+    'prefer-const': 'warn',
+    'no-var': 'error',
+    'eqeqeq': ['error', 'always'],
+    'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 1 }],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'quotes': ['warn', 'single', { avoidEscape: true }],
+    'semi': ['warn', 'always'],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['warn'],
+      },
+    },
+    {
+      files: ['*.test.ts', '*.test.tsx', '*.spec.ts', '*.spec.tsx'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
